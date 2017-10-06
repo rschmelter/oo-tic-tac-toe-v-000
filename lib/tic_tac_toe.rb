d27e3f9ec375686ddb2ba92df3fa2238fcfe25a9
Ryan Schmelter <rschmelter33@gmail.com>
2017-10-06 00:38:10 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -13,7 +13,7 @@ class TicTacToe
     [0, 4, 8],
     [2, 4, 6]
   ]
-  def display_board
+  def display_board(board)
     puts " #{board[0]} " + "|" + " #{board[1]} " + "|" + " #{board[2]} "
     puts "-----------"
     puts " #{board[3]} " + "|" + " #{board[4]} " + "|" + " #{board[5]} "

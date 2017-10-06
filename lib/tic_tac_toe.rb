adf1dd33e4b90e176a0ae2f577bf85bf23c16d70
Ryan Schmelter <rschmelter33@gmail.com>
2017-10-06 00:37:27 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -20,7 +20,7 @@ class TicTacToe
     puts "-----------"
     puts " #{board[6]} " + "|" + " #{board[7]} " + "|" + " #{board[8]} "
   end
-  
+
   def input_to_index(input)
     input.to_i - 1
   end
